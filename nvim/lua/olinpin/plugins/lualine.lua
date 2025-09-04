92d85071defeb9a5e5531ff2c4cd77e4287c7113
oliverhnat <oliver.hnat@gmail.com>
2025-09-04 14:19:12 +0200

    feat(ui): redesign lualine to have same background as nvim theme

--- nvim/lua/olinpin/plugins/lualine.lua
+++ nvim/lua/olinpin/plugins/lualine.lua
@@ -2,26 +2,31 @@ return {
 	"nvim-lualine/lualine.nvim",
 	event = "VeryLazy",
 	requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	opts = {
-		options = {
-			theme = "ayu_mirage",
-		},
-		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = {
-				{
-					"filename",
-					file_status = true,
-					path = 1,
-					shorting_target = 40,
+	init = function()
+		local lualine = require("lualine")
+		local custom_ayu_mirage = require("lualine.themes.ayu_mirage")
+		custom_ayu_mirage.normal.c.bg = "#172c35"
+		lualine.setup({
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						path = 1,
+						shorting_target = 40,
+					},
 				},
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
-			lualine_x = { "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-	},
+			options = {
+				theme = custom_ayu_mirage,
+			},
+		})
+	end,
 }
